f43e64f6b98a7db99ec360748bedeadaff01f88e
Tim van den Belt <info@timvandenbelt.com>
2022-02-03 15:21:48 +0100

    Generated default scaffolding for pages

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Page;
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Page $page)
+    {
+        //
+    }
+
+    public function edit(Page $page)
+    {
+        //
+    }
+
+    public function update(Request $request, Page $page)
+    {
+        //
+    }
+
+    public function destroy(Page $page)
+    {
+        //
+    }
+}
--- database/migrations/2022_01_22_233616_create_pages_table.php
+++ database/migrations/2022_01_22_233616_create_pages_table.php
@@ -6,20 +6,30 @@ use Illuminate\Support\Facades\Schema;
 
 class CreatePagesTable extends Migration
 {
-    public function up()
-    {
-        Schema::create('pages', function (Blueprint $table)
-        {
-            $table->bigIncrements('id');
-
-            //
-
-            $table->timestamps();
-        });
-    }
-
     public function down()
     {
-        Schema::dropIfExists('pages');
+        Schema::dropIfExists("pages");
+    }
+
+    public function up()
+    {
+        Schema::create("pages", function (Blueprint $table) {
+            $table->id();
+
+            $table->string("title")->unique();
+            $table
+                ->string("slug")
+                ->unique()
+                ->index();
+
+            $table->mediumText("body");
+            $table->text("excerpt");
+
+            $table->boolean("published");
+            $table->dateTime("published_at");
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
     }
 }
